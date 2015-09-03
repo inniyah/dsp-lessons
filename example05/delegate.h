99efd903bb930adfd480300006bd7699ea37a219
Miriam Ruiz <miriam@debian.org>
2015-09-03 18:28:38 +0200

    5th Example

---
+++ example05/delegate.h
@@ -0,0 +1,48 @@
+#ifndef DELEGATE_H_
+#define DELEGATE_H_
+
+#include <cstdlib>
+
+// See: http://blog.coldflake.com/posts/C++-delegates-on-steroids/
+
+#if __cplusplus <= 199711L // not a C++11 compliant compiler
+#error At least a C++11 compliant compiler is needed
+#endif
+
+// Usage:
+//     A a;
+//     auto d = Delegate<int, int>::from_function<A, &A::foo>(&a);
+//     auto d2 = Delegate<int, int, int, char>::from_function<A, &A::bar>(&a);
+
+template<typename return_type, typename... params>
+class Delegate {
+
+	typedef return_type (*Type)(void* callee, params...);
+
+public:
+
+	Delegate(void * callee, Type function) : fpCallee(callee) , fpCallbackFunction(function) { }
+
+	template <class T, return_type (T::*TMethod)(params...)>
+	static Delegate fromObjectMethod(T * callee) {
+		Delegate d(callee, &methodCaller<T, TMethod>);
+		return d;
+	}
+
+	return_type operator()(params... xs) const {
+		return (*fpCallbackFunction)(fpCallee, xs...);
+	}
+
+private:
+
+	void * fpCallee;
+	Type fpCallbackFunction;
+
+	template <class T, return_type (T::*TMethod)(params...)>
+	static return_type methodCaller(void* callee, params... xs) {
+		T * p = static_cast<T*>(callee);
+		return (p->*TMethod)(xs...);
+	}
+};
+
+#endif // DELEGATE_H_
---
+++ example05/example05.cpp
@@ -0,0 +1,91 @@
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <unistd.h>
+#include "delegate.h"
+
+typedef Delegate<double> GeneratorDelegate;
+
+class PulseGenerator {
+public:
+	PulseGenerator(int period, double amplitude = 1.0) :
+		m_Counter(0),
+		m_Period(period),
+		m_Amplitude(amplitude)
+	{
+	}
+	double get() {
+		return ( ((m_Counter++) % m_Period) < (m_Period/2) ? m_Amplitude : 0.0 );
+	}
+
+private:
+	int m_Counter;
+	int m_Period;
+	double m_Amplitude;
+};
+
+class RampGenerator {
+public:
+	RampGenerator(int period, double amplitude = 1.0) :
+		m_Counter(0),
+		m_Period(period),
+		m_Amplitude(amplitude)
+	{
+	}
+	double get() {
+		return ( ((m_Counter++) % m_Period) * m_Amplitude / (m_Period-1) );
+	}
+
+private:
+	int m_Counter;
+	int m_Period;
+	double m_Amplitude;
+};
+
+class Filter {
+	/*
+	 * first order IIR filters to approximate a K sample moving average.
+	 * This function implements the equation:
+	 *
+	 *                  y[n] = alpha * x[n] + (1 - alpha) * y[n-1]
+	 *
+	 * This filter approximates a moving average of the last K samples
+	 * by setting the value of alpha to 1/K.
+	 *
+	 * See: http://electronics.stackexchange.com/questions/30370/fast-and-memory-efficient-moving-average-calculation
+	 * See: http://dsp.stackexchange.com/questions/378/what-is-the-best-first-order-iir-approximation-to-a-moving-average-filter
+	 */
+public:
+	Filter(GeneratorDelegate generator, double alpha = 0.5) :
+		m_GeneratorDelegate(generator),
+		m_Alpha(alpha),
+		m_Output(0.0)
+	{
+	}
+	virtual double get() {
+		double input = m_GeneratorDelegate();
+		m_Output = m_Alpha * input + (1.0 - m_Alpha) * m_Output;
+		return m_Output;
+	}
+
+private:
+	GeneratorDelegate m_GeneratorDelegate;
+	double m_Alpha;
+	double m_Output;
+};
+
+int main(int argc, const char * argv[]) {
+	unsigned long int Tick = 0;
+	PulseGenerator  pgen(8);
+	Filter          pflt(GeneratorDelegate::fromObjectMethod<PulseGenerator, &PulseGenerator::get>(&pgen));
+	RampGenerator   rgen(8);
+	Filter          rflt(GeneratorDelegate::fromObjectMethod<RampGenerator, &RampGenerator::get>(&rgen));
+	while (true) {
+		printf("A\t%lu\t%lf\n", Tick, pflt.get());
+		printf("B\t%lu\t%lf\n", Tick, rflt.get());
+		++Tick;
+		usleep(100 * 1000lu);
+	}
+	return EXIT_SUCCESS;
+}
